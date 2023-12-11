790db7591cf3952922ebf1cab78d8286558a175d
carleandro7 <carleandro7@gmail.com>
2023-12-11 10:53:29 -0300

    adicionado menu bar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenubarComponent } from './component/menubar/menubar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
+  declarations: [
+    AppComponent,
+    MenubarComponent
+  ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
