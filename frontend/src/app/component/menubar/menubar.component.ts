5dae1c2455a77eab95a13dfd52e3b412aca9c521
carleandro7 <carleandro7@gmail.com>
2023-12-11 12:17:03 -0300

    teste de formulario

--- frontend/src/app/component/menubar/menubar.component.ts
+++ frontend/src/app/component/menubar/menubar.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup  } from '@angular/forms';
 
 @Component({
   selector: 'app-menubar',
   templateUrl: './menubar.component.html',
   styleUrls: ['./menubar.component.scss']
 })
-export class MenubarComponent {
+export class MenubarComponent implements OnInit {
+  public countrylist=['India','USA','Singapore','UK']
+  public termlist=['15days','30days','45days','60days']
   badgevisible = true;
+
+  constructor(private builder:FormBuilder){
+
+  }
+  ngOnInit(): void {
+    this.customerform.setValue({name:'Nihira Techiees',email:'nihiratechiees@gmail.com',phone:'77678899',
+  country:'USA',term:'45days',address:'add1',dob:new Date(2001,2,3),gender:'Male',status:true})
+  }
+
+  public customerform=this.builder.group({
+    name:this.builder.control('',Validators.required),
+    email:this.builder.control('',Validators.compose([Validators.required,Validators.required]) ),
+    phone:this.builder.control('',Validators.required),
+    country:this.builder.control('',Validators.required),
+    address:this.builder.control('',Validators.required),
+    term:this.builder.control('',Validators.required),
+    dob:this.builder.control(new Date(2000,3,25)),
+    gender:this.builder.control('Male'),
+    status:this.builder.control(true),
+   });
+
   badgevisibility() {
     this.badgevisible = false;
   }
+
+  SaveCustomer(){
+    console.log(this.customerform.value);
+   }
+ 
+   clearform(){
+     this.customerform.reset();
+   }
 }
